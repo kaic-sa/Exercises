0a1f080ec26f2ea4d82f5123ebd6434999f4559b
kaic-sa <kaictacker@gmai.com>
2022-07-20 09:18:32 -0300

    last editions

---
+++ Javascript/howOld.js
@@ -0,0 +1,25 @@
+const whatRelation = percentSharedDNA => {
+    if (percentSharedDNA === 100) {
+
+        return 'You are likely identical twins.'
+    } else if (percentSharedDNA >= 35) {
+        return 'You are likely parent and child or full siblings.'
+    } else if (percentSharedDNA >= 14) {
+        return 'You are likely grandparent and grandchild, aunt/uncle and niece/nephew, or half siblings.'
+    } else if (percentSharedDNA >= 6) {
+        return 'You are likely 1st cousins.'
+    } else if (percentSharedDNA >= 3) {
+        return 'You are likely 2st cousins.'
+    } else if (percentSharedDNA > 0) {
+        return 'You are likely 3nd cousins.'
+    } else if (percentSharedDNA === 0) {
+        return 'You are likely not related.'
+    }
+
+}
+
+console.log(whatRelation(34))
+    // Should print 'You are likely grandparent and grandchild, aunt/uncle and niece/nephew, or half siblings.'
+
+console.log(whatRelation(3))
+    // Should print 'You are likely 2nd cousins.'
---
+++ number_guesser/number-guesser-starting/script1.js
@@ -0,0 +1,4 @@
+const n = [1, 2, 3, 4, 5]
+
+n = [22];
+console.log(n);
